4193e21ee0a5ce80ad54154e8127c93dfdfcade9
graziella01 <amberbambico.54@gmail.com>
2025-11-18 18:50:09 +0800

    Initial commit: Travel List React App

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
     </div>
+  );
+}
+
+function Logo() {
+  return <h1>🌴 Far Away 🧳</h1>;
+}
+function Form() {
+  return (
+    <div>
+      <h3>What do you need for your trip?</h3>
+      <form>
+        <select>
+          {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => {
+            return <option value={num}>{num}</option>;
+          })}
+        </select>
+        <input type="text" placeholder="Item..." />
+        <button>Add</button>
+      </form>
+    </div>
+  );
+}
+function PackingList() {}
+function Stats() {
+  return (
+    <footer>
+      💼 You have 0 items on your list, and you are ready to go! ✈️
+    </footer>
   );
 }
 
